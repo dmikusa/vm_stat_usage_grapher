0416f58c5de09188695599ec97f6c54f8c835d4b
Daniel Mikusa <dmikusa@pivotal.io>
2015-07-12 19:43:59 -0400

    Added install script for project.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='usage_grapher',
+      version='0.1',
+      description='Memory usage grapher',
+      author='Daniel Mikusa',
+      author_email='dan@mikusa.com',
+      license='Apache 2',
+      packages=['usage_grapher'])
